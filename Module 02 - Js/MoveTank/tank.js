a49563fdc3c4dbe2a7556461b33a4333a35b7a1b
Daryii <112935597+Daryii@users.noreply.github.com>
2023-04-10 18:14:18 +0200

    commit

--- Module 02 - Js/MoveTank/tank.js
+++ Module 02 - Js/MoveTank/tank.js
@@ -20,8 +20,33 @@ function checkKey(e) {
     }
 }
 
-function animateScript() {
-    document.getElementById("image").style.backgroundPosition = 
-    `80px 80px`;
+var tID; //we will use this variable to clear the setInterval()
 
-}
+function stopAnimate() {
+  clearInterval(tID);
+} //end of stopAnimate()
+
+
+function animateScript() {
+
+  var position = 164; //start position for the image slicer
+  const interval = 100; //100 ms of interval for the setInterval()
+  const diff = 80; //diff as a variable for position offset
+  
+  tID = setInterval(() => {
+  
+    document.getElementById("image").style.backgroundPosition =
+    `${position}px 0px`;
+    //we use the ES6 template literal to insert the variable "position"
+    
+    if (position < 2560) {
+      position = position + diff;
+    }
+    //we increment the position by 256 each time
+    else {
+      position = 80;
+    }
+    //reset the position to 256px, once position exceeds 1536px
+    
+  }, interval); //end of setInterval
+} //end of animateScript()
