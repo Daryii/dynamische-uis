b639160726c0e2e1a7be988f06f727ca8080d466
Daryii <112935597+Daryii@users.noreply.github.com>
2024-04-22 15:49:47 +0200

    Commit!

--- Labs/Gamepicker/script.js
+++ Labs/Gamepicker/script.js
@@ -1,13 +1,198 @@
 // Spellenarray
 const games = [
-  { title: "Game 1", price: 29.99, genre: "Action", rating: 4.5 },
-  { title: "Game 2", price: 39.99, genre: "Adventure", rating: 4.0 },
-  { title: "Game 3", price: 24.99, genre: "RPG", rating: 4.2 },
-  { title: "Game 4", price: 49.99, genre: "Shooter", rating: 4.7 },
-  { title: "Game 5", price: 19.99, genre: "Platformer", rating: 3.8 },
-  { title: "Game 6", price: 34.99, genre: "Puzzle", rating: 4.1 },
-  { title: "Game 7", price: 29.99, genre: "Simulation", rating: 4.3 },
-  { title: "Game 8", price: 44.99, genre: "Strategy", rating: 4.6 },
+  {
+      "title": "Counter-Strike: Global Offensive",
+      "price": 0.00,
+      "genre": "FPS",
+      "rating": 4
+  },
+  {
+      "title": "Dota 2",
+      "price": 0.00,
+      "genre": "MOBA",
+      "rating": 3
+  },
+  {
+      "title": "Goose Goose Duck",
+      "price": 4.99,
+      "genre": "Action",
+      "rating": 2
+  },
+  {
+      "title": "Apex Legends",
+      "price": 0.00,
+      "genre": "FPS",
+      "rating": 4
+  },
+  {
+      "title": "PUBG: BATTLEGROUNDS",
+      "price": 29.99,
+      "genre": "FPS",
+      "rating": 5
+  },
+  {
+    "title": "Lost Ark",
+    "price": 49.99,
+    "genre": "Action",
+    "rating": 1
+},
+{
+    "title": "Grand Theft Auto V",
+    "price": 29.99,
+    "genre": "FPS",
+    "rating": 3
+},
+{
+    "title": "Call of Duty®: Modern Warfare® II | Warzone™ 2.0",
+    "price": 19.99,
+    "genre": "FPS",
+    "rating": 3
+},
+{
+    "title": "Team Fortress 2",
+    "price": 0.00,
+    "genre": "FPS",
+    "rating": 5
+},
+{
+    "title": "Rust",
+    "price": 39.99,
+    "genre": "Action",
+    "rating": 5
+},
+{
+    "title": "Unturned",
+    "price": 0.00,
+    "genre": "RPG",
+    "rating": 1
+},
+{
+    "title": "ELDEN RING",
+    "price": 59.99,
+    "genre": "RPG",
+    "rating": 5
+},
+{
+    "title": "ARK: Survival Evolved",
+    "price": 10.00,
+    "genre": "RPG",
+    "rating": 1
+},
+{
+    "title": "War Thunder",
+    "price": 0.00,
+    "genre": "Simulation",
+    "rating": 2
+},
+{
+  "title": "Sid Meier's Civilization VI",
+  "price": 29.99,
+  "genre": "Simulation",
+  "rating": 3
+},
+{
+  "title": "Football Manager 2023",
+  "price": 59.99,
+  "genre": "Simulation",
+  "rating": 3
+},
+{
+  "title": "Warframe",
+  "price": 0.00,
+  "genre": "Looter-shooter",
+  "rating": 3
+},
+{
+  "title": "EA SPORTS™ FIFA 23",
+  "price": 59.99,
+  "genre": "Sport",
+  "rating": 1
+},
+{
+  "title": "Destiny 2",
+  "price": 0.00,
+  "genre": "FPS",
+  "rating": 5
+},
+{
+  "title": "Red Dead Redemption 2",
+  "price": 59.99,
+  "genre": "RPG",
+  "rating": 4
+},
+{
+  "title": "Tom Clancy's Rainbow Six Siege",
+  "price": 19.99,
+  "genre": "Simulation",
+  "rating": 3
+},
+{
+  "title": "The Witcher 3: Wild Hunt",
+  "price": 39.99,
+  "genre": "RPG",
+  "rating": 4
+},
+{
+  "title": "Terraria",
+  "price": 9.99,
+  "genre": "Sandbox",
+  "rating": 2
+},
+{
+  "title": "Stardew Valley",
+  "price": 14.99,
+  "genre": "Sandbox",
+  "rating": 1
+},
+{
+  "title": "Left 4 Dead 2",
+  "price": 9.99,
+  "genre": "FPS",
+  "rating": 4
+},
+{
+  "title": "Don't Starve Together",
+  "price": 5.09,
+  "genre": "RPG",
+  "rating": 3
+},
+{
+  "title": "MIR4",
+  "price": 19.99,
+  "genre": "RPG",
+  "rating": 3
+},
+{
+  "title": "PAYDAY 2",
+  "price": 9.99,
+  "genre": "Action",
+  "rating": 2
+},
+{
+  "title": "Path of Exile",
+  "price": 0.00,
+  "genre": "RPG",
+  "rating": 4
+},
+{
+  "title": "Project Zomboid",
+  "price": 14.99,
+  "genre": "Simulation",
+  "rating": 4
+},
+{
+  "title": "Valheim",
+  "price": 19.99,
+  "genre": "Sandbox",
+  "rating": 5
+},
+{
+  "title": "DayZ",
+  "price": 44.99,
+  "genre": "Simulation",
+  "rating": 3
+}
+  // Add the rest of your JSON game data here
 ];
 
 const addedGames = []; // Array to store added games
